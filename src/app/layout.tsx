9267516477a5a1e80b4e8ffc1643e90e09d8bdcf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-22 10:15:29 +0600

    dropdown menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "title",
+  title: "Islamic School",
   description: "description",
 };
 
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -8,35 +8,84 @@ import ActiveLink from "../ui/ActiveLink";
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
+  const navItems = (
+    <>
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/">Home</ActiveLink>
+      </li>
+      <li className="relative group cursor-pointer ">
+        <p className="hover:text-yellow-600 transition-colors duration-500">
+          Faculties
+        </p>
+        <ul className="absolute top-10 left-0 bg-[#1b1464] p-4 space-y-2 opacity-0 transform -translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 duration-300 transition-all w-44 rounded-xl border-y-[3px] border-yellow-600 pointer-events-none group-hover:pointer-events-auto z-[999]">
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/quran">Quran</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/hadith">Hadith</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/fiqh">Fiqh</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/islamic-history">
+              Islamic History
+            </ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/islamic">Islamic</ActiveLink>
+          </li>
+        </ul>
+      </li>
+      <li className="relative group cursor-pointer">
+        <span className="hover:text-yellow-600 transition-colors duration-500">
+          Courses
+        </span>
+        <ul className="absolute top-10 left-0 bg-[#1b1464] p-4 space-y-2 opacity-0 transform -translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 duration-300 transition-all w-44 rounded-xl border-y-[3px] border-yellow-600 pointer-events-none group-hover:pointer-events-auto z-[999]">
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/quran">Quran</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/hadith">Hadith</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/fiqh">Fiqh</ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/islamic-history">
+              Islamic History
+            </ActiveLink>
+          </li>
+          <li className="hover:text-yellow-600 transition-colors duration-500">
+            <ActiveLink href="/faculties/islamic">Islamic</ActiveLink>
+          </li>
+        </ul>
+      </li>
+
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/islamic-library">Islamic Library</ActiveLink>
+      </li>
+
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/islamic-qa">Islamic Q&A</ActiveLink>
+      </li>
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/blog">Blog</ActiveLink>
+      </li>
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/contact">Contact</ActiveLink>
+      </li>
+      <li className="hover:text-yellow-600 transition-colors duration-500">
+        <ActiveLink href="/about">About</ActiveLink>
+      </li>
+    </>
+  );
+
   return (
     <nav className="py-5 px-7 md:px-[4%] bg-[#1b1464] text-white flex justify-between items-center relative">
       <h2 className="text-[28px] font-semibold">Logo</h2>
       <ul className="hidden md:flex gap-4 text-[16px] md:text-[17px] font-medium">
-        <li className="hover:text-yellow-600 transition-colors duration-500">
-          <ActiveLink href="/">Home</ActiveLink>
-        </li>
-        <li className="hover:text-yellow-600 transition-colors duration-500">
-          <ActiveLink href="/about">About</ActiveLink>
-        </li>
-        <li className=" relative group cursor-pointer">
-          <span className="hover:text-yellow-600 transition-colors duration-500">
-            Portfolio
-          </span>
-          <ul className="absolute top-10 left-0 bg-[#1b1464] p-4 space-y-2 opacity-0 transform -translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 duration-300 transition-all w-44 rounded-xl border-y-[3px] border-yellow-600">
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              One he fhe
-            </li>
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              Two
-            </li>
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              Three
-            </li>
-          </ul>
-        </li>
-        <li className="hover:text-yellow-600 transition-colors duration-500">
-          <ActiveLink href="/contact">Contact</ActiveLink>
-        </li>
+        {navItems}
       </ul>
 
       <div className="hidden md:block">
@@ -56,36 +105,10 @@ const Navbar = () => {
 
         <div
           className={`absolute ${
-            isNavOpen ? "top-20" : "-top-[250px]"
+            isNavOpen ? "top-20" : "-top-[450px]"
           }  right-0 bg-[#1b1464] w-full p-7 transition-all duration-500 border-t-2 border-yellow-600 rounded-t`}
         >
-          <ul className="space-y-4 text-[16px] md:text-[17px]">
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              <ActiveLink href="/">Home</ActiveLink>
-            </li>
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              <ActiveLink href="/about">About</ActiveLink>
-            </li>
-            <li className=" relative group cursor-pointer">
-              <span className="hover:text-yellow-600 transition-colors duration-500">
-                Portfolio
-              </span>
-              <ul className="absolute top-10 left-0 bg-[#1b1464] p-4 space-y-2 opacity-0 transform -translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 duration-300 transition-all w-44 rounded-xl border-y-[3px] border-yellow-600">
-                <li className="hover:text-yellow-600 transition-colors duration-500">
-                  One he fhe
-                </li>
-                <li className="hover:text-yellow-600 transition-colors duration-500">
-                  Two
-                </li>
-                <li className="hover:text-yellow-600 transition-colors duration-500">
-                  Three
-                </li>
-              </ul>
-            </li>
-            <li className="hover:text-yellow-600 transition-colors duration-500">
-              <ActiveLink href="/contact">Contact</ActiveLink>
-            </li>
-          </ul>
+          <ul className="space-y-4 text-[16px] md:text-[17px]">{navItems}</ul>
         </div>
       </div>
     </nav>
